a7ec70355e379bb89748d4cd7fe8ba04aa6264d5
Daniela Murai <dany.murai@gmail.com>
2025-07-23 16:46:34 -0300

    adicionando fixtures para credenciais valida e invalida

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,14 +1,17 @@
+
 describe('Login', () => {
   beforeEach(() => {
     // Arrange
     cy.visit('http://localhost:4000')
-    
+
   });
 
   it('Login com dados válidos deve permitir entrada no sistema', () => {
     // Act
-    cy.get('#username').click().type('julio.lima')
-    cy.get('#senha').click().type('123456')
+    cy.fixture('credenciais').then(credenciais => {
+      cy.get('#username').click().type(credenciais.valida.usuario)
+      cy.get('#senha').click().type(credenciais.valida.senha)
+    })
     cy.contains('button', 'Entrar').click()
 
     // Assert
@@ -17,8 +20,11 @@ describe('Login', () => {
 
   it('Login com dados inválidos deve apresentar mensagem de erro', () => {
     // Act
-    cy.get('#username').click().type('julio.lima')
-    cy.get('#senha').click().type('654321')
+    cy.fixture('credenciais').then(credenciais => {
+      cy.get('#username').click().type(credenciais.invalida.usuario)
+      cy.get('#senha').click().type(credenciais.invalida.senha)
+    })
+
     cy.contains('button', 'Entrar').click()
 
     // Assert
