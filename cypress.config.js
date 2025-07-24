6a3d5c8b7016f7d9f0180df9b1a7b9ae34a1c3d2
Daniela Murai <dany.murai@gmail.com>
2025-07-24 10:33:21 -0300

    alterando a origen da url

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  baseUrl: 'http://localhost:4000',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
